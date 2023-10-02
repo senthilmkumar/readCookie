2bc8c14bcb78fb7797d96ac498fa22bf4392f388
nomadicCoder <senthilkumar.muniappan@kp.org>
2023-10-01 22:58:11 -0700

    Updated actions file

--- bin/getSiteCookie.py
+++ bin/getSiteCookie.py
@@ -1,20 +1,35 @@
 import requests
+import json
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+os.chdir(__location__)
+os.system('clear')
+
 
 print("Running through gihub actions")
 
 # URL of the website you want to check
-url = 'https://google.com'
+#url = 'https://google.com'
 
-# Send an HTTP GET request to the website
-response = requests.get(url)
+with open('../config/sites.json') as f:
+    data = json.load(f)
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Extract and print the cookies from the response
-    cookies = response.cookies
-    for cookie in cookies:
-        print(f"Cookie Name: {cookie.name}")
-        print(f"Cookie Value: {cookie.value}")
-else:
-    print(f"Failed to fetch the website. Status code: {response.status_code}")
+for site, url in data.items():
+    print(f"{site}: {url}")
+
+    # Send an HTTP GET request to the website
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Extract and print the cookies from the response
+        cookies = response.cookies
+        for cookie in cookies:
+            print(f"Cookie Name: {cookie.name}")
+            print(f"Cookie Value: {cookie.value}")
+    else:
+        print(f"Failed to fetch the website. Status code: {response.status_code}")
 
